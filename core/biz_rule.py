ecf44467772bf1c60dfcc61f9e50969c86ed5c45
Muying Zhao <muying.zhao31@gmail.com>
2025-09-15 20:31:45 +0100

    initial solution to get marvel comic books data

--- core/biz_rule.py
+++ core/biz_rule.py
@@ -1,7 +1,13 @@
 import requests
 from core.models import Service, Field, Object,Form, TextForm, IntegerForm, FloatForm, BooleanForm, DateForm, URLForm, CharacterForm
 from datetime import datetime
+#show detailed error messages
 import traceback
+# The secret should be added to gitignore, but keep it here for running the code
+from core.secret import api_key
+import hashlib
+
+
 # Error handling omitted for https requests for brevity
 def _empty_all():
     form_list = [TextForm, IntegerForm, FloatForm, BooleanForm, DateForm, URLForm, CharacterForm]
@@ -17,6 +23,11 @@ def _date_converter(date_str):
     formatted = parsed.strftime("%Y-%m-%d")
     return formatted
 
+def _date_converter_marvel(date_str):
+
+    parsed = datetime.strptime(date_str, "%Y-%m-%d")
+    formatted = parsed.strftime("%Y-%m-%d")
+    return formatted
 
 FORM_TYPE_MAP = {
     Field.CHAR: CharacterForm,
@@ -27,8 +38,6 @@ FORM_TYPE_MAP = {
     Field.DATE: DateForm,
     Field.URL: URLForm,
 }
-
-
 
 
 def _add_field_value(obj: Object, field: Field, value):
@@ -53,10 +62,13 @@ def _add_field(obj: Object, service: Service, name: str, description: str, form_
 
 def _pokenmon_data():
     # This url gets a list of all pokemon card sets
+    api_key = api_key.get("poken_key","")
     base_pokemon_url = "https://api.pokemontcg.io/v2/sets"
+
     headers = {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "X-Api-Key": api_key
     }
     response = requests.get(f"{base_pokemon_url}", headers=headers)
     print(f"Finished API Call, {str(response.status_code)}")
@@ -116,14 +128,91 @@ def _pokenmon_data():
             value=pokemon_set.get("images",{"symbol":""}).get("symbol","")
         )
 
+def _marvel_data():
+    # variables for the API call
+    # URL, API KEY
+    comics_marvel_url = "https://gateway.marvel.com/v1/public/comics"
+    public_key = api_key.get("marvel_public_key","")
+    private_key = api_key.get("marvel_private_key","")
+    ts = str(datetime.now().timestamp())
+    # hash — a md5 digest of the ts parameter, your private key and your public key (e.g, md5(ts+privateKey+publicKey)
+    hash_input = ts + private_key + public_key
+    hash_result = hashlib.md5(hash_input.encode()).hexdigest()
+    params = { "apikey": public_key, "ts": ts, "hash": hash_result}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+
+    # make the request to get the data 
+    response = requests.get(f"{comics_marvel_url}", headers=headers, params=params)
+    print(f"Finished API Call, {str(response.status_code)}")
+    
+
+    # create a service, which is the virutal table 
+    service = Service.objects.create(
+        name="MarvelComicsCollection",
+        description="Collection of Marvel Comics books",
+    )
+
+    created_objects = [] 
+    for marvel_comic in response.json()["data"]["results"]:
+        # create an object for each marvel comic book, like a row in the datatable
+        obj = Object(service=service)
+        obj.save() 
+        created_objects.append(obj.human_id)
+        # add fields to the object, like columns in the datatable
+        _add_field(
+            obj=obj,
+            service=service,
+            name="title",
+            description="Name of the Marvel comic book",
+            form_type=Field.TEXT,
+            value=marvel_comic.get("title","")
+        )
+        _add_field(
+            obj=obj,
+            service=service,
+            name="pageCount",
+            description="The number of pages of the Marvel comic book",
+            form_type=Field.TEXT,
+            value=marvel_comic.get("pageCount","")
+        )
+        _add_field(
+            obj=obj,
+            service=service,
+            name="resourceURI",
+            description="The resource URI of the Marvel comic book",
+            form_type=Field.URL,
+            value=marvel_comic.get("resourceURI","")
+        )
+        # _add_field(
+        #     obj=obj,
+        #     service=service,
+        #     name="focDate",
+        #     description=" Final Order Cutoff date of the Marvel comic book",
+        #     form_type=Field.DATE,
+        #     value= _date_converter_marvel([date for date in marvel_comic["dates"] if date["type"] == "focDate"][0].get("date",""))
+        # )
+        _add_field(
+            obj=obj,
+            service=service,
+            name="price",
+            description="The print price of the Marvel comic book",
+            form_type=Field.FLOAT,
+            value= [price for price in marvel_comic["prices"] if price["type"] == "printPrice"][0].get("price",0.0)
+        )
         
+
 def main():
     # 
     try:
-        _pokenmon_data()
+        #_pokenmon_data()
+        _marvel_data()
     except Exception as e:
         print(e)
         print(traceback.format_exc())
         print("delete everything")
         # only delete the created objects in this round if an error occurs
-        _empty_all()
+        #_empty_all()
---
+++ core/secret.py
@@ -0,0 +1,5 @@
+api_key = {
+    "poken_key": "eeff6c24-e3b6-4553-8357-fe116ff106c6",
+    "marvel_public_key": "3e663fe077de3ed64e2d398f49da1bad",
+    "marvel_private_key": "7546bfbfb85613c243fca64400a03c5df75e26b4"
+}
